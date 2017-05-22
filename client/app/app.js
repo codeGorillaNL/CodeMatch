41d1c72108ea008ec002555268084c484d585f72
Ramon CodeGorilla <r.horst@codegorilla.nl>
2017-05-22 20:13:28 +0200

    navbar updates

--- client/app/app.js
+++ client/app/app.js
@@ -2,7 +2,7 @@
 
 angular.module('codeGorillaApp', ['codeGorillaApp.auth', 'codeGorillaApp.admin',
     'codeGorillaApp.constants', 'ngCookies', 'ngResource', 'ngSanitize', 'ui.router',
-    'validation.match', 'angularMaterialFormBuilder', 'ngMaterial', 'duScroll', 'ngMdIcons'
+    'validation.match', 'angularMaterialFormBuilder', 'ngMaterial', 'duScroll', 'ngMdIcons', 'uiGmapgoogle-maps'
   ])
   .config(function($urlRouterProvider, $locationProvider) {
     $urlRouterProvider.otherwise('/');
