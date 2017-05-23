48684246548ea33caf21d2329d315417a1abcc22
Ramonvankoll <ramonvankoll@hotmail.com>
2017-05-23 11:45:35 +0200

    Cleaned last JS SDK from linkedin

--- client/app/app.js
+++ client/app/app.js
@@ -4,19 +4,13 @@ angular.module('codeGorillaApp', ['codeGorillaApp.auth', 'codeGorillaApp.admin',
     'codeGorillaApp.constants', 'ngCookies', 'ngResource', 'ngSanitize', 'ui.router',
     'validation.match', 'angularMaterialFormBuilder', 'ngMaterial', 'duScroll', 'ngMdIcons',
     'socialLogin'
-  ])
-  .config(function($urlRouterProvider, $locationProvider) {
-    $urlRouterProvider.otherwise('/');
+    ])
 
+.config(function($urlRouterProvider, $locationProvider) {
+    $urlRouterProvider.otherwise('/');
     $locationProvider.html5Mode(true);
-  });
-  app.config(function(socialProvider){
+    });
+
+app.config(function(socialProvider){
     socialProvider.setLinkedInKey("867xyimscli5cm");
-    $rootScope.$on('event:social-sign-in-success', function(event, userDetails)
-    {
-        name: <user_name>,
-        email: <user_email>,
-        imageUrl: <image_url>,
-        uid: LinkedIN
-    })
-  });
+    });
