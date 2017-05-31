22b848a2b745d22f226573a35b5b8a91d3be044c
codeZillaNL <bert@codegorilla.nl>
2017-05-31 10:36:03 +0200

    updated

--- client/app/app.js
+++ client/app/app.js
@@ -4,7 +4,8 @@ angular.module('codeGorillaApp', ['codeGorillaApp.auth', 'codeGorillaApp.admin',
     'codeGorillaApp.constants', 'ngCookies', 'ngResource', 'ngSanitize', 'ui.router',
     'validation.match', 'angularMaterialFormBuilder', 'ngMaterial', 'duScroll', 'ngMdIcons', 'uiGmapgoogle-maps'
   ])
-  .config(function($urlRouterProvider, $locationProvider) {
+  .config(function($urlRouterProvider, $locationProvider, $uiViewScrollProvider) {
     $urlRouterProvider.otherwise('/');
     $locationProvider.html5Mode(true);
+    $uiViewScrollProvider.useAnchorScroll();
     });
